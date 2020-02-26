631c85600409548eaecc18206bfb262f66e26e21
hierbe <xiaosilei@qq.com>
2020-02-27 02:42:08 +0800

    机构列表逻辑

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 from apps.organizations.models import CourseOrg, City
 
 class OrgView(View):
     def get(self, request, *args, **kwargs):
         all_orgs = CourseOrg.objects.all()
         org_nums = CourseOrg.objects.count()
-        all_city = City.objects.all()
+        all_citys = City.objects.all()
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, per_page=10, request=request)
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_orgs' : all_orgs,
+            'all_orgs' : orgs,
             'org_nums' : org_nums,
-            'all_city' : all_city,
+            'all_citys' : all_citys,
         })
